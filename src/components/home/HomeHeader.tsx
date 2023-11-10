ed6daef03497ce92c28203fd0bfe9475871ef2e1
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2023-11-10 23:55:23 +0900

    Fix: header resize font

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -3,40 +3,40 @@ import React from 'react';
 
 export default function HomeHeader() {
   return (
-    <header className="border-b border-b-gray-200">
-      <div className="container mx-auto flex items-center justify-between text-gray-400 fill-gray-400 p-1">
+    <header className="border-b border-b-gray200">
+      <div className="w-11/12 container mx-auto flex items-center justify-between text-zinc-400 fill-gray-400 p-1">
         <div className="flex items-center">
-          <a
-            className="mr-4 flex items-center gap-x-2 transition-colors current"
-            href="/"
-          >
-            <h1 className="text-3xl text-black font-sans font-thin">Ryama</h1>
+          <a className="flex items-center transition-colors" href="/">
+            <h1 className="mx-4 text-2xl text-black font-sans font-thin">
+              RYAMA
+            </h1>
           </a>
         </div>
         <div className="ml-auto hidden sm:flex">
           <a
-            className="px-3 py-2 font-medium hover:text-black transition"
+            className="px-3 font-medium text-xs hover:text-black transition"
             href="/public"
           >
             PUBLIC
           </a>
           <a
-            className="px-3 py-2 font-medium hover:text-black transition"
+            className="px-3 font-medium text-xs hover:text-black transition"
             href="/features"
           >
             FEATURES
           </a>
           <a
-            className="px-3 py-2 font-medium hover:text-black transition"
+            className="px-3 font-medium text-xs hover:text-black transition"
             href="/signup"
           >
             SIGN UP
           </a>
           <a
-            className="px-3 py-2 font-bold hover:text-black transition flex items-center"
+            className="px-3 font-semibold text-xs hover:text-black transition flex items-center"
             href="/signin"
           >
-            SIGN IN <AiOutlineArrowRight />
+            <span className="mr-1">SIGN IN</span>
+            <AiOutlineArrowRight />
           </a>
         </div>
       </div>
