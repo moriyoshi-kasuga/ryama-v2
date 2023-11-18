ebddfa1ab8dcde1f4cc911748cbb7ea2e86619c5
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2023-11-18 21:01:50 +0900

    Fix: set database type to supabase and create migrations

---
+++ src/lib/schema.ts
@@ -0,0 +1,312 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[];
+
+export interface Database {
+  graphql_public: {
+    Tables: {
+      [_ in never]: never;
+    };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      graphql: {
+        Args: {
+          operationName?: string;
+          query?: string;
+          variables?: Json;
+          extensions?: Json;
+        };
+        Returns: Json;
+      };
+    };
+    Enums: {
+      [_ in never]: never;
+    };
+    CompositeTypes: {
+      [_ in never]: never;
+    };
+  };
+  public: {
+    Tables: {
+      profiles: {
+        Row: {
+          avatar_url: string;
+          id: string;
+          name: string;
+        };
+        Insert: {
+          avatar_url: string;
+          id: string;
+          name: string;
+        };
+        Update: {
+          avatar_url?: string;
+          id?: string;
+          name?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: 'profiles_id_fkey';
+            columns: ['id'];
+            referencedRelation: 'users';
+            referencedColumns: ['id'];
+          },
+        ];
+      };
+    };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      [_ in never]: never;
+    };
+    Enums: {
+      [_ in never]: never;
+    };
+    CompositeTypes: {
+      [_ in never]: never;
+    };
+  };
+  storage: {
+    Tables: {
+      buckets: {
+        Row: {
+          allowed_mime_types: string[] | null;
+          avif_autodetection: boolean | null;
+          created_at: string | null;
+          file_size_limit: number | null;
+          id: string;
+          name: string;
+          owner: string | null;
+          owner_id: string | null;
+          public: boolean | null;
+          updated_at: string | null;
+        };
+        Insert: {
+          allowed_mime_types?: string[] | null;
+          avif_autodetection?: boolean | null;
+          created_at?: string | null;
+          file_size_limit?: number | null;
+          id: string;
+          name: string;
+          owner?: string | null;
+          owner_id?: string | null;
+          public?: boolean | null;
+          updated_at?: string | null;
+        };
+        Update: {
+          allowed_mime_types?: string[] | null;
+          avif_autodetection?: boolean | null;
+          created_at?: string | null;
+          file_size_limit?: number | null;
+          id?: string;
+          name?: string;
+          owner?: string | null;
+          owner_id?: string | null;
+          public?: boolean | null;
+          updated_at?: string | null;
+        };
+        Relationships: [];
+      };
+      migrations: {
+        Row: {
+          executed_at: string | null;
+          hash: string;
+          id: number;
+          name: string;
+        };
+        Insert: {
+          executed_at?: string | null;
+          hash: string;
+          id: number;
+          name: string;
+        };
+        Update: {
+          executed_at?: string | null;
+          hash?: string;
+          id?: number;
+          name?: string;
+        };
+        Relationships: [];
+      };
+      objects: {
+        Row: {
+          bucket_id: string | null;
+          created_at: string | null;
+          id: string;
+          last_accessed_at: string | null;
+          metadata: Json | null;
+          name: string | null;
+          owner: string | null;
+          owner_id: string | null;
+          path_tokens: string[] | null;
+          updated_at: string | null;
+          version: string | null;
+        };
+        Insert: {
+          bucket_id?: string | null;
+          created_at?: string | null;
+          id?: string;
+          last_accessed_at?: string | null;
+          metadata?: Json | null;
+          name?: string | null;
+          owner?: string | null;
+          owner_id?: string | null;
+          path_tokens?: string[] | null;
+          updated_at?: string | null;
+          version?: string | null;
+        };
+        Update: {
+          bucket_id?: string | null;
+          created_at?: string | null;
+          id?: string;
+          last_accessed_at?: string | null;
+          metadata?: Json | null;
+          name?: string | null;
+          owner?: string | null;
+          owner_id?: string | null;
+          path_tokens?: string[] | null;
+          updated_at?: string | null;
+          version?: string | null;
+        };
+        Relationships: [
+          {
+            foreignKeyName: 'objects_bucketId_fkey';
+            columns: ['bucket_id'];
+            referencedRelation: 'buckets';
+            referencedColumns: ['id'];
+          },
+        ];
+      };
+    };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      can_insert_object: {
+        Args: {
+          bucketid: string;
+          name: string;
+          owner: string;
+          metadata: Json;
+        };
+        Returns: undefined;
+      };
+      extension: {
+        Args: {
+          name: string;
+        };
+        Returns: string;
+      };
+      filename: {
+        Args: {
+          name: string;
+        };
+        Returns: string;
+      };
+      foldername: {
+        Args: {
+          name: string;
+        };
+        Returns: unknown;
+      };
+      get_size_by_bucket: {
+        Args: Record<PropertyKey, never>;
+        Returns: {
+          size: number;
+          bucket_id: string;
+        }[];
+      };
+      search: {
+        Args: {
+          prefix: string;
+          bucketname: string;
+          limits?: number;
+          levels?: number;
+          offsets?: number;
+          search?: string;
+          sortcolumn?: string;
+          sortorder?: string;
+        };
+        Returns: {
+          name: string;
+          id: string;
+          updated_at: string;
+          created_at: string;
+          last_accessed_at: string;
+          metadata: Json;
+        }[];
+      };
+    };
+    Enums: {
+      [_ in never]: never;
+    };
+    CompositeTypes: {
+      [_ in never]: never;
+    };
+  };
+}
+
+// Schema: graphql_public
+// Functions
+export type ArgsGraphql =
+  Database['graphql_public']['Functions']['graphql']['Args'];
+export type ReturnTypeGraphql =
+  Database['graphql_public']['Functions']['graphql']['Returns'];
+
+// Schema: public
+// Tables
+export type Profiles = Database['public']['Tables']['profiles']['Row'];
+export type InsertProfiles = Database['public']['Tables']['profiles']['Insert'];
+export type UpdateProfiles = Database['public']['Tables']['profiles']['Update'];
+
+// Schema: storage
+// Tables
+export type Buckets = Database['storage']['Tables']['buckets']['Row'];
+export type InsertBuckets = Database['storage']['Tables']['buckets']['Insert'];
+export type UpdateBuckets = Database['storage']['Tables']['buckets']['Update'];
+
+export type Migrations = Database['storage']['Tables']['migrations']['Row'];
+export type InsertMigrations =
+  Database['storage']['Tables']['migrations']['Insert'];
+export type UpdateMigrations =
+  Database['storage']['Tables']['migrations']['Update'];
+
+export type Objects = Database['storage']['Tables']['objects']['Row'];
+export type InsertObjects = Database['storage']['Tables']['objects']['Insert'];
+export type UpdateObjects = Database['storage']['Tables']['objects']['Update'];
+
+// Functions
+export type ArgsCanInsertObject =
+  Database['storage']['Functions']['can_insert_object']['Args'];
+export type ReturnTypeCanInsertObject =
+  Database['storage']['Functions']['can_insert_object']['Returns'];
+
+export type ArgsExtension =
+  Database['storage']['Functions']['extension']['Args'];
+export type ReturnTypeExtension =
+  Database['storage']['Functions']['extension']['Returns'];
+
+export type ArgsFilename = Database['storage']['Functions']['filename']['Args'];
+export type ReturnTypeFilename =
+  Database['storage']['Functions']['filename']['Returns'];
+
+export type ArgsFoldername =
+  Database['storage']['Functions']['foldername']['Args'];
+export type ReturnTypeFoldername =
+  Database['storage']['Functions']['foldername']['Returns'];
+
+export type ArgsGetSizeByBucket =
+  Database['storage']['Functions']['get_size_by_bucket']['Args'];
+export type ReturnTypeGetSizeByBucket =
+  Database['storage']['Functions']['get_size_by_bucket']['Returns'];
+
+export type ArgsSearch = Database['storage']['Functions']['search']['Args'];
+export type ReturnTypeSearch =
+  Database['storage']['Functions']['search']['Returns'];
---
+++ supabase/migrations/20231118120002_add: profile.sql
@@ -0,0 +1,45 @@
+create table public.profiles (
+  id uuid not null references auth.users on delete cascade,
+  name text not null,
+  avatar_url text not null,
+
+  primary key (id)
+);
+
+alter table public.profiles enable row level security;
+
+create policy "Public profiles are viewable by everyone."
+  on profiles for select
+  using ( true );
+
+create policy "Users can insert their own profile."
+  on profiles for insert
+  with check ( auth.uid() = id );
+
+create policy "Users can update own profile."
+  on profiles for update
+  using ( auth.uid() = id );
+
+
+-- inserts a row into public.profiles
+create function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer set search_path = public
+as $$
+begin
+  insert into public.profiles (id,name,avatar_url)
+  values (
+    new.id,
+    new.raw_user_meta_data::json->>'name',
+    new.raw_user_meta_data::json->>'avatar_url'
+  );
+  return new;
+end;
+$$;
+
+-- trigger the function every time a user is created
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
+
